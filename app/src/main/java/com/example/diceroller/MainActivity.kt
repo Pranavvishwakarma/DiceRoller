f900a2d34f2a59298e9132c3dcb8bd43dca00bc8
Pranavvishwakarma <pranav213234@gmail.com>
2022-08-23 13:52:16 +0530

    Update

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,56 @@
+
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+        }
+    }
+
+
+
+class Dice(private val numSides: Int) {
+
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
